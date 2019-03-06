e3fc43620b95eb3560188e17a3dd98c176dc9e13
Ashley Bland <ashleyjbland@gmail.com>
2019-03-06 23:48:09 +0000

    Automatically backed up by Learn

---
+++ app/application.rb
@@ -0,0 +1,19 @@
+class Application
+
+  def call(env)
+    resp = Rack::Response.new
+
+    time = timestamp
+
+
+
+    if time < 12:00
+      resp.write "Good Morning!"
+    else
+      resp.write "Good Afternoon!"
+    end
+
+    resp.finish
+  end
+
+end
